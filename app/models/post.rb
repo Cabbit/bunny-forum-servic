395d6d83b9996dc2e3b61b073951915beb1b63d8
David Roy <david.roy@cabbit.co.uk>
2016-07-17 01:40:45 +0100

    Adds method for updating forum posts count

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 class Post < ActiveRecord::Base
   belongs_to :topic
-  after_create { increment_topic_replies_count! }
+  after_create do
+    increment_topic_replies_count!
+    increment_forum_post_count!
+  end
 
   validates :title, presence: true
 
   def increment_topic_replies_count!
     topic.increment!(:replies_count, 1)
+  end
+
+  def increment_forum_post_count!
+    forum.increment!(:posts_count, 1)
+  end
+
+  private
+
+  def forum
+    @forum ||= topic.forum
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,12 +4,19 @@ require_relative '../spec_helper'
 class PostSpec < TestCase
   let(:subject) { Post.find(1) }
   let(:topic)   { Topic.find(1) }
+  let(:forum)   { Forum.find(1) }
 
   describe '#after_create' do
     it 'should update the topic replies counter by 1' do
       Post.create!(title: 'A', topic_id: 1)
 
       assert_equal 2, topic.replies_count
+    end
+
+    it 'should update the forums\'s post counter by 1' do
+      Post.create!(title: 'A', topic_id: 1)
+
+      assert_equal 2, forum.posts_count
     end
   end
 
@@ -18,6 +25,12 @@ class PostSpec < TestCase
       subject.increment_topic_replies_count!
 
       assert_equal 2, topic.replies_count
+    end
+
+    it 'should update the forums\'s post counter by 1' do
+      subject.increment_forum_post_count!
+
+      assert_equal 2, forum.posts_count
     end
   end
 end
